702caaf8c129a78b1c50217c9808669bd701e7c5
BratuAndreiEugen <andreieugenbratu@gmail.com>
2024-04-07 16:08:46 +0300

    email notifications

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import requests
 import time
 import datetime
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 
 def parse_headers(filename: str):
     """
@@ -100,11 +105,24 @@ for date in date_list_str:
     date_list.append(int(date))
 print(date_list)
 
+sent = {}
+
+# opening config files
+with open("configs/generalConfig.json") as json_data_file:
+    general_config = json.load(json_data_file)
+
+with open("configs/gheorgheniConfig.json") as json_data_file:
+    specific_config = json.load(json_data_file)
+
+print(general_config)
+print(specific_config)
+
+
 while True:
     i = 1
     while i <= 14:
-        url = "https://www.calendis.ro/api/get_available_slots?service_id=8029&location_id=1651&date=" + str(unix_timestamp_midnight) + "&day_only=1"
-        custom_headers = parse_headers("header_files/header_file_1.txt")
+        url = specific_config["base_request_url"] + str(unix_timestamp_midnight) + "&day_only=1"
+        custom_headers = parse_headers(general_config["header_file_path"])
         # print(custom_headers)
 
         response = send_api_request(url, headers=custom_headers)
@@ -123,12 +141,39 @@ while True:
 
             day = int(gmt_time.strftime("%d"))
             print(day)
-            if day in date_list:
-                # Message box should be shown here
-                pass
+
+            # Send email to all recipients IF something changed to the available times
+            if day in date_list and (day not in sent or sent[day] != output_str):
+                sent[day] = output_str
+
+                # Email credentials
+                email_address = general_config["email_address"]
+                password = general_config["app_password"]
+
+                # Email details
+                recipients = general_config["recipients"]
+                subject = 'Am gasit liber in data ' + gmt_time.strftime("%d %m %y")
+                body = output_str + "\n\n " + specific_config["res_human_url"]
+
+                # Create message container
+                msg = MIMEMultipart()
+                msg['From'] = email_address
+                msg['To'] = ', '.join(recipients)
+                msg['Subject'] = subject
+
+                msg.attach(MIMEText(body, 'plain'))
+
+                with smtplib.SMTP(general_config["smtp_server"], 587) as server:
+                    server.starttls()  # Secure the connection
+                    # Login to email server
+                    server.login(email_address, password)
+                    # Send email
+                    server.send_message(msg)
+
 
         print()
         i=i+1
         unix_timestamp_midnight = increment_unix_timestamp(unix_timestamp_midnight)
 
-    time.sleep(30)
+    # time.sleep(30)
+    unix_timestamp_midnight = get_unix_timestamp_midnight()
